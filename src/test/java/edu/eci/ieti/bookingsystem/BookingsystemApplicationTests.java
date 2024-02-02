1edf3956b9f62eb8c9104306cf8e156fe0b613f5
Daniel Santanilla <daniel1sa_@hotmail.com>
2024-02-02 11:30:23 -0500

    Configuración inicial proyecto integrador

---
+++ src/test/java/edu/eci/ieti/bookingsystem/BookingsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.ieti.bookingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
